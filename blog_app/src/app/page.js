5fa61f45613bf39c300fe4afc0fa8da8d32250c0
Chetna <cmaske580@gmail.com>
2024-08-10 21:51:23 +0530

    Card Component Completed

--- blog_app/src/app/page.js
+++ blog_app/src/app/page.js
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
@@ -10,6 +12,7 @@ export default function Home({ searchParams }) {
       <Featured />
       <CategoryList />
       <div className={styles.content}>
+        <CardList page={page} />
       </div>
     </div>
   );
--- blog_app/src/components/card/Card.jsx
+++ blog_app/src/components/card/Card.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import styles from "./card.module.css";
+import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       {item.img && (
         <div className={styles.imageContainer}>
           <Image src="./p1.jpeg" alt="" fill className={styles.image} />
@@ -12,12 +13,22 @@ const Card = () => {
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
           </span>
           <span className={styles.category}>{item.catSlug}</span>
         </div>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
+        </Link>
+        {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
 };
 
 export default Card;
+
