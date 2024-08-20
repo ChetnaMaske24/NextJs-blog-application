1ff699aff9dabef4979958b6959b83395029c708
Chetna <cmaske580@gmail.com>
2024-08-20 16:53:57 +0530

    Comments.jsx

--- blog_app/src/app/posts/[slug]/page.jsx
+++ blog_app/src/app/posts/[slug]/page.jsx
@@ -1,21 +1,45 @@
+import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Comments from "@/components/comments/Comments";
 
-const SinglePage = () => {
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.content}>
-            <div className={styles.post}>
-              <div
-                className={styles.description}
-                dangerouslySetInnerHTML={{ __html: data?.desc }}
-              />
-              <div className={styles.comment}></div>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae doloremque aut hic ut iste, neque veritatis inventore possimus, laboriosam officia perferendis</h1>
+          <div className={styles.user}>
+              <div className={styles.userImageContainer}>
+                <Image src="./p1.jpg" alt="" fill className={styles.avatar} />
+              </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>Chetna Maske</span>
+              <span className={styles.date}>20.08.2024</span>
             </div>
           </div>
         </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src="./p1.jpg" alt="" fill className={styles.image} />
+          </div>
+        )}
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+          <div className={styles.comment}>
+            <Comments postSlug={slug}/>
+          </div>
+        </div>
+        <Menu />
       </div>
     </div>
   );
